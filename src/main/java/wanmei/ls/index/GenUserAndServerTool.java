fb163f417ef6f916951535b90499211676be1598
thegodofwar <thegodofwar9@gmail.com>
2012-09-06 23:14:32 +0800

    delete and new

--- src/main/java/wanmei/ls/index/GenUserAndServerTool.java
+++
@@ -1,9 +0,0 @@
-package wanmei.ls.index;
-
-public class GenUserAndServerTool {
-	/*
-	 * 暂时设计一台IS对应一台LS
-	 * 数据迁移的实现以后再说，预计为IS LS一起迁移。保持LS和IS的一对一
-	 * 这种情况下这个Tool不需要
-	 * */
-}
--- src/main/java/wanmei/utils/wordsegment/Segmentor.java
+++
@@ -1,9 +0,0 @@
-package wanmei.utils.wordsegment;
-
-
-public interface Segmentor {
-
-	public TermInfo[] doIndexSegment(String input);
-	public TermInfo[] doQuerySegment(String input);
-	public void close();
-}
